db378234b5aa5ab0cc9ad8f587925e141ae8a5b9
Clopezgg <chlopezg9@gmail.com>
2026-07-17 13:40:08 -0600

    deploy: add Coolify websocket entrypoint

---
+++ deploy/coolify/start-websocket.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd /home/frappe/frappe-bench
+bash apps/erpnext/deploy/coolify/configure-site.sh common
+bash apps/erpnext/deploy/coolify/wait-for-site.sh
+exec node apps/frappe/socketio.js
