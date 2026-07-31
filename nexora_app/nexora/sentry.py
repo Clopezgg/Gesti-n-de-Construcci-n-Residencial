f58564815796f2b7a56fff24a9776e2064b22695
CARLOS HUMBERTO LOPEZ GONZALEZ <chlopezg9@gmail.com>
2026-07-31 17:08:31 -0600

    Update sentry.py

--- nexora_app/nexora/sentry.py
+++ nexora_app/nexora/sentry.py
@@ -4,12 +4,12 @@ import sentry_sdk
 
 
 def init_sentry():
-    sentry_sdk.init(
-        dsn=os.getenv(
-            "SENTRY_DSN",
-            "https://a0925cf9dcdc3feae0e225bb50d04fa1@o4511830522265600.ingest.us.sentry.io/4511830661529600",
-        ),
-        send_default_pii=True,
-        traces_sample_rate=1.0,
-        environment="nexora",
-    )
+	sentry_sdk.init(
+		dsn=os.getenv(
+			"SENTRY_DSN",
+			"https://a0925cf9dcdc3feae0e225bb50d04fa1@o4511830522265600.ingest.us.sentry.io/4511830661529600",
+		),
+		send_default_pii=True,
+		traces_sample_rate=1.0,
+		environment="nexora",
+	)
