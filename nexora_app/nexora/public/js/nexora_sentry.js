f3b2c2e87d5ca8da7e9001e22c7404bca9fa3ef2
CARLOS HUMBERTO LOPEZ GONZALEZ <chlopezg9@gmail.com>
2026-07-31 15:50:31 -0600

    style(lint): apply prettier formatting to nexora_sentry.js

--- nexora_app/nexora/public/js/nexora_sentry.js
+++ nexora_app/nexora/public/js/nexora_sentry.js
@@ -1,35 +1,17 @@
 /* eslint-disable no-undef */
 /* global Sentry */
-(function(){
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://browser.sentry-cdn.com/10.1.0/bundle.min.js";
+  script.onload = function () {
+    Sentry.init({
+      dsn: "https://a0925cf9dcdc3feae0e225bb50d04fa1@o4511830522265600.ingest.us.sentry.io/4511830661529600",
+      environment: "nexora",
+      tracesSampleRate: 1.0,
+    });
 
-    var script=document.createElement("script");
+    console.log("NEXORA SENTRY FRONTEND ACTIVE");
+  };
 
-    script.src=
-    "https://browser.sentry-cdn.com/10.1.0/bundle.min.js";
-
-
-    script.onload=function(){
-
-        Sentry.init({
-
-            dsn:"https://a0925cf9dcdc3feae0e225bb50d04fa1@o4511830522265600.ingest.us.sentry.io/4511830661529600",
-
-            environment:"nexora",
-
-            tracesSampleRate:1.0
-
-        });
-
-
-        console.log(
-        "NEXORA SENTRY FRONTEND ACTIVE"
-        );
-
-
-    };
-
-
-    document.head.appendChild(script);
-
-
+  document.head.appendChild(script);
 })();
