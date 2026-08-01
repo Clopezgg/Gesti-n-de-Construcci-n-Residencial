011f400368b5519cfdcda2c83f8e404b7b1e6f55
CARLOS HUMBERTO LOPEZ GONZALEZ <chlopezg9@gmail.com>
2026-08-01 02:00:55 +0000

    style(ci): apply prettier/ruff formatting to sentry hooks

--- nexora_app/nexora/public/js/nexora_sentry.js
+++ nexora_app/nexora/public/js/nexora_sentry.js
@@ -1,34 +1,20 @@
 /* global Sentry */
-(function(){
+(function () {
+	var script = document.createElement("script");
 
-    var script=document.createElement("script");
+	script.src = "https://browser.sentry-cdn.com/10.1.0/bundle.min.js";
 
-    script.src=
-    "https://browser.sentry-cdn.com/10.1.0/bundle.min.js";
+	script.onload = function () {
+		Sentry.init({
+			dsn: "https://a0925cf9dcdc3feae0e225bb50d04fa1@o4511830522265600.ingest.us.sentry.io/4511830661529600",
 
+			environment: "nexora",
 
-    script.onload=function(){
+			tracesSampleRate: 1.0,
+		});
 
-        Sentry.init({
+		console.log("NEXORA SENTRY FRONTEND ACTIVE");
+	};
 
-            dsn:"https://a0925cf9dcdc3feae0e225bb50d04fa1@o4511830522265600.ingest.us.sentry.io/4511830661529600",
-
-            environment:"nexora",
-
-            tracesSampleRate:1.0
-
-        });
-
-
-        console.log(
-        "NEXORA SENTRY FRONTEND ACTIVE"
-        );
-
-
-    };
-
-
-    document.head.appendChild(script);
-
-
+	document.head.appendChild(script);
 })();
